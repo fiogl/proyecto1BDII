aae438b9b22fb5703d82a242819b0e021e46c812
Fiorella González López <fiorellagonzalezlopez28@gmail.com>
2025-09-16 19:31:43 -0600

    Creación inicial (vacío)

---
+++ demoproyecto1BDII/src/main/java/com/oracle/demoproyecto1BDII/Demoproyecto1BdiiApplication.java
@@ -0,0 +1,14 @@
+package com.oracle.demoproyecto1BDII;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBootApplication
+public class Demoproyecto1BdiiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(Demoproyecto1BdiiApplication.class, args);
+	}
+}
---
+++ demoproyecto1BDII/src/test/java/com/oracle/demoproyecto1BDII/Demoproyecto1BdiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracle.demoproyecto1BDII;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demoproyecto1BdiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
